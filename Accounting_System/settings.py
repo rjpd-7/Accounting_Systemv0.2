e396e65216f2024aaff476213929b0d3d69d710e
rjpd7 <d.roeljoseph@gmail.com>
2026-02-12 22:48:27 +0800

    minor changes for hosting

--- Accounting_System/settings.py
+++ Accounting_System/settings.py
@@ -119,15 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
 import os
 
-# For PythonAnywhere, use absolute path; for local development, use relative
-if 'pythonanywhere' in os.getenv('SERVER_SOFTWARE', ''):
-    STATIC_ROOT = '/home/rjpd7/rjpd7.pythonanywhere.com/static'
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/rjpd7/rjpd7.pythonanywhere.com/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
